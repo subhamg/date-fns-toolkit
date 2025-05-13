fdb7f28267ad9aa59d216d49c12fd53e56b2a562
Subham Goyal <subham.iitk@gmail.com>
2025-05-13 16:20:30 +0530

    Update Jest configuration to use CommonJS syntax and add type definition for improved compatibility with TypeScript.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-export default {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
